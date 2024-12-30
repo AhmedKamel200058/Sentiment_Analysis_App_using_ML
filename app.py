5e4adc252fb40a8070c69144f00570154c291500
Ahmed Mohamed Kamel AbdElTawab <125600290+AhmedKamel200058@users.noreply.github.com>
2024-12-30 14:40:01 +0200

    Last_UPDATE

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ if state:
                 balloon_class  = "balloon positive"
                 st.balloons()
             else:
-                balloon_class  = "Snow negative"
+                balloon_class  = "balloon negative"
                 st.snow()
 
             # Display the sentiment in a styled balloon
